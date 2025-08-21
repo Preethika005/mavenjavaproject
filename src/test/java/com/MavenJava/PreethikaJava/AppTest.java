0bc5088247ed153775f28304eb328a2695d5fad0
Preethika005 <preethikar3005@gmail.com>
2025-08-21 22:14:14 +0530

    initial commit

---
+++ src/test/java/com/MavenJava/PreethikaJava/AppTest.java
@@ -0,0 +1,19 @@
+package com.MavenJava.PreethikaJava;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
